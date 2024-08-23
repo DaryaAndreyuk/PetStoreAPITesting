f49cbcff17a9e4a359c5d4eab2b0346a078317f7
darya <dasha.andreyuk@gmail.com>
2024-08-23 16:45:08 +0200

    add simple POST and GET tests

--- src/test/java/SimpleAPITest.java
+++ src/test/java/SimpleAPITest.java
@@ -1,14 +1,57 @@
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 
 public class SimpleAPITest {
 
-    private static final String BASE_URL = "https://petstore.swagger.io/#/";
+    private static final String BASE_URL = "https://petstore.swagger.io/v2";
+    private static final String PET_ID = "9223372016900028000";
 
     @Test
-    public void simpleGetInventoryTest() {
-        String endpoint = BASE_URL + "store/getInventory";
-        given().when().get(endpoint).then().log().all();
+    void getPetTest() {
+        String endpoint = BASE_URL + "/pet/" + PET_ID;
+
+        var response = given().
+                header("accept", "application/json").
+                when().
+                get(endpoint).
+                then();
+
+        response.log().body();
+        response.statusCode(200);
     }
 
+    @Test
+    void createPetTest() {
+        String endpoint = BASE_URL + "/pet";
+        String body = """
+                {
+                  "id": 0,
+                  "category": {
+                    "id": 0,
+                    "name": "cat"
+                  },
+                  "name": "cattie",
+                  "photoUrls": [
+                    "string"
+                  ],
+                  "tags": [
+                    {
+                      "id": 0,
+                      "name": "string"
+                    }
+                  ],
+                  "status": "available"
+                }
+                """;
+        var response = given().
+                header("accept", "application/json").
+                header("Content-Type", "application/json").
+                body(body).
+                when().
+                post(endpoint).
+                then();
+        response.log().body();
+        response.statusCode(200);
+    }
 }
