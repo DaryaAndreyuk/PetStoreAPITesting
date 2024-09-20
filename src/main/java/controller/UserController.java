f52405fd192e000952460aa2b47bfdbc05655768
darya <dasha.andreyuk@gmail.com>
2024-09-20 19:45:37 +0200

    refactor tests to use controller methods

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -6,6 +6,10 @@ import io.restassured.http.Method;
 import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import models.User;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static utils.Constants.*;
@@ -31,6 +35,62 @@ public class UserController {
                 .when()
                 .body(DEFAULT_USER)
                 .request(Method.POST, USER_ENDPOINT)
+                .then()
+                .log().ifError()
+                .extract()
+                .response();
+    }
+
+    public Response addDefaultUserWithArray(){
+        User user1 = new User(1, "user1", "Sam", "Smith", "sam.smith@example.com", "password123", "1234567890", 1);
+        User user2 = new User(2, "user2", "Harry", "Styles", "harry.styles@example.com", "password456", "0987654321", 2);
+        List<User> usersArray = Arrays.asList(DEFAULT_USER, user1, user2);
+
+        return given()
+                .header(ACCEPT_HEADER, APP_JSON_TYPE)
+                .header(CONTENT_TYPE_HEADER, APP_JSON_TYPE)
+                .body(usersArray)
+                .when()
+                .post(BASE_URL + "/user/createWithArray")
+                .then()
+                .log().ifError()
+                .extract()
+                .response();
+    }
+
+    public Response logoutUser(){
+        return given()
+                .header(ACCEPT_HEADER, APP_JSON_TYPE)
+                .when()
+                .get(BASE_URL + "/user/logout")
+                .then()
+                .log().ifError()
+                .extract()
+                .response();
+    }
+
+    public Response loginUser(){
+        String username = "valid_username";
+        String password = "valid_password";
+
+        return given()
+                .header(ACCEPT_HEADER, APP_JSON_TYPE)
+                .queryParam("username", username)
+                .queryParam("password", password)
+                .when()
+                .get(BASE_URL + "/user/login")
+                .then()
+                .log().ifError()
+                .extract()
+                .response();
+    }
+
+    public Response deleteUser(String username){
+        return given()
+                .header(ACCEPT_HEADER, APP_JSON_TYPE)
+                .header(CONTENT_TYPE_HEADER, APP_JSON_TYPE)
+                .when()
+                .delete(BASE_URL + "/user/" + username)
                 .then()
                 .log().ifError()
                 .extract()
--- src/test/java/UserSmokeTests.java
+++ src/test/java/UserSmokeTests.java
@@ -9,10 +9,6 @@ import static io.restassured.RestAssured.given;
 import static utils.Constants.*;
 
 public class UserSmokeTests extends BaseTest {
-
-    private static final String USER_ENDPOINT = BASE_URL + "/user";
-    private static final String NON_EXIST_USERNAME_ENDPOINT = USER_ENDPOINT + "/non_existing_username";
-    private static final String NON_EXIST_USER_ENDPOINT = USER_ENDPOINT + "/user_not_existed";
     UserController userController = new UserController();
 
     @Test
@@ -24,67 +20,28 @@ public class UserSmokeTests extends BaseTest {
 
     @Test
     public void createUsersWithArrayTest() {
-        User user1 = new User(1, "user1", "Sam", "Smith", "sam.smith@example.com", "password123", "1234567890", 1);
-        User user2 = new User(2, "user2", "Harry", "Styles", "harry.styles@example.com", "password456", "0987654321", 2);
-        List<User> usersArray = Arrays.asList(user1, user2);
-
-        Response response = given()
-                .header(ACCEPT_HEADER, APP_JSON_TYPE)
-                .header(CONTENT_TYPE_HEADER, APP_JSON_TYPE)
-                .body(usersArray)
-                .when()
-                .post(BASE_URL + "/user/createWithArray")
-                .then()
-                .log().ifError()
-                .extract()
-                .response();
-
+        Response response = userController.addDefaultUserWithArray();
         response.prettyPrint();
-        Assert.assertEquals(response.statusCode(), 200, "User creation with array failed!");
+        Assert.assertEquals(response.statusCode(), 200);
     }
 
     @Test
     public void logoutUserTest() {
-        Response response = given()
-                .header(ACCEPT_HEADER, APP_JSON_TYPE)
-                .when()
-                .get(BASE_URL + "/user/logout")
-                .then()
-                .log().ifError()
-                .extract()
-                .response();
-
+        Response response = userController.logoutUser();
         response.prettyPrint();
-        Assert.assertEquals(response.statusCode(), 200, "Logout operation failed!");
-
+        Assert.assertEquals(response.statusCode(), 200);
         Assert.assertEquals(response.jsonPath().getInt("code"), 200);
         Assert.assertEquals(response.jsonPath().getString("message"), "ok");
     }
 
     @Test
-    public void successfulLoginTest() {
-        String username = "valid_username";
-        String password = "valid_password";
-
-        Response response = given()
-                .header(ACCEPT_HEADER, APP_JSON_TYPE)
-                .queryParam("username", username)
-                .queryParam("password", password)
-                .when()
-                .get(BASE_URL + "/user/login")
-                .then()
-                .log().ifError()
-                .extract()
-                .response();
-
+    public void loginTest() {
+        Response response = userController.loginUser();
         Assert.assertEquals(response.statusCode(), 200, "Login operation failed!");
-
         String expiresAfter = response.getHeader("X-Expires-After");
         String rateLimit = response.getHeader("X-Rate-Limit");
-
         Assert.assertNotNull(expiresAfter, "X-Expires-After header is missing!");
         Assert.assertNotNull(rateLimit, "X-Rate-Limit header is missing!");
-
         System.out.println("Login successful. X-Expires-After: " + expiresAfter + ", X-Rate-Limit: " + rateLimit);
         response.prettyPrint();
     }
@@ -93,19 +50,8 @@ public class UserSmokeTests extends BaseTest {
     public void deleteExistingUserAAATest() {
         Response addUserResponse = userController.addDefaultUser();
         addUserResponse.prettyPrint();
-
         String username = addUserResponse.jsonPath().getString("username");
-
-        Response deleteResponse = given()
-                .header(ACCEPT_HEADER, APP_JSON_TYPE)
-                .header(CONTENT_TYPE_HEADER, APP_JSON_TYPE)
-                .when()
-                .delete(BASE_URL + "/user/" + username)
-                .then()
-                .log().ifError()
-                .extract()
-                .response();
-
+        Response deleteResponse = userController.deleteUser(username);
         Assert.assertEquals(deleteResponse.statusCode(), 200, "User deletion failed!");
         deleteResponse.prettyPrint();
     }
@@ -174,7 +120,6 @@ public class UserSmokeTests extends BaseTest {
     @Test
     public void getExistingUserAAATest() {
         Response addResponse = userController.addDefaultUser();
-
         Response getResponse = userController.findUser(DEFAULT_USER.getUsername());
         User getUser = getResponse.as(User.class);
         getResponse.prettyPrint();
